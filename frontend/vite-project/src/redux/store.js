8ece7e3877a9dae1f20307d207cde777764fcef5
Md Hasib Zaman <md383494@dal.ca>
2025-06-13 23:52:50 -0300

    Remove vite-project folder to fix build and clean up project

--- frontend/vite-project/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import productReducer from "./reducers/productReducers";
-import { thunk } from "redux-thunk";
-
-const rootReducer = combineReducers({
-  products: productReducer,
-});
-
-export const store = createStore(rootReducer, applyMiddleware(thunk));
